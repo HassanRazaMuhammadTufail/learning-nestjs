0cfd90a28dc9b7789ea8b9d6255a3c05bb62968f
hassan raza <hassanrazamohammadtufail@gmail.com>
2020-06-04 17:52:27 +0500

    interceptor added

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { logger } from './middlewares/logger.middleware';
 import { HttpExceptionFilter } from './exceptionHandlers/http-exception.filter'; 
 import { ValidationPipe } from './pipes/validation.pipe';
+import { LoggingInterceptor } from './interceptors/logging.interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalPipes(new ValidationPipe());
   //handling globally
   app.useGlobalFilters(new HttpExceptionFilter());
