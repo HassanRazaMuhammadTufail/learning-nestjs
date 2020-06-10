450484d331e1c7bd83b64e03c1d1911b9c929a49
hassan raza <hassanrazamohammadtufail@gmail.com>
2020-06-10 12:51:20 +0500

    auth tachnique with passportJs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,26 @@
-import { Controller, Get, Req, Post, HttpCode, Redirect,Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Req,
+  Post,
+  HttpCode,
+  Redirect,
+  Param,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AppService } from './app.service';
-import {Request} from 'express';
+import { Request as request } from 'express';
+import { User } from './customDecorators/user.decorator';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
 
-@Controller({host: 'localhost'})
+/**
+ * supplied host property to controller decorator
+ */
+@Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,private authService: AuthService) {}
 
   @Get()
   getHello(): string {
@@ -12,18 +28,27 @@ export class AppController {
   }
   @Get('redirect')
   @Redirect('https://hassanrazamohdtufail.web.app/', 302)
-  getHassan(@Req() request: Request): string {
+  getHassan(@Req() request: request): string {
     return this.appService.getName(request);
   }
   @Post()
   @HttpCode(200)
-  create(@Req() request: Request) :object {
-    console.log(request)
-    return {data:request.route}
+  create(@Req() request: request): object {
+    console.log(request);
+    return { data: request.route };
   }
-//   @Get(':id')
-// findOne(@Param() params): string {
-//   console.log(params.id);
-//   return `This action returns a #${params.id} cat`;
-// }
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+  //   @Get(':id')
+  // findOne(@Param() params): string {
+  //   console.log(params.id);
+  //   return `This action returns a #${params.id} cat`;
+  // }
+  @Get('custom')
+  async findOne(@User('firstName') firstName: string) {
+    console.log(`Hello ${firstName}`);
+  }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { LoggerMiddleware, logger } from './middlewares/logger.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
